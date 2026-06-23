a8b8d140ddd4d8cd1174fd239475e0ba9b703203
shauryapratap1210 <shauryapratapsinghs1210@gmail.com>
2026-06-23 13:24:08 +0530

    Added Scroll Reveal Animation and Scroll Up

--- js/main.js
+++ js/main.js
@@ -81,9 +81,54 @@ contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        } else {
+            sectionsClass.classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+
+    distance: '60px',
+    origin: 'top',
+    duration: 2500,
+
+    // reset:true
+})
+
+sr.reveal(`.home__content, .resume__content:nth-child(1)`)
+sr.reveal(`.home__data,.resume__content:nth-child(2)`, { delay: 300, origin: 'bottom' })
+
+sr.reveal(`.about__content, .contact__content`, { origin: 'bottom' })
+sr.reveal(`.about__image, .contact__form`, { delay: 300 })
+
+
+sr.reveal(`.projects__card`, { interval: 100 })
